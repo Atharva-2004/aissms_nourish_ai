10b92faddd1d7605e3064822758bd3437027a93a
Atharva Ajagekar <atharva7ajagekar@gmail.com>
2025-02-25 19:30:03 +0530

    chatbot import

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -24,8 +24,8 @@ import Recipe_Bot from './pages/Recipe_Bot/Recipe_Bot.jsx';
 import SingleMealStatus from './Components/Consumer/SingleMealStatus.jsx';
 //import Google_Translate from './Components/Google_Translate.jsx';
 import NgoCurrentAcceptPage from './Components/NGO/ngocurrentaccept.jsx';
-import ChatBot from './Components/Chat-Bot/Chat-Bot.jsx';
 
+import ChatBot from './Components/Chat-Bot/Chat-Bot.jsx';
 
 function App() {
   return (
